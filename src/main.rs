20b56ab972013259f4ab9f59b39fc782561b8600
andreykaravaev640 <andreykaravaev640@gmail.com>
2026-04-30 16:28:01 +0800

    Templates and their Rust implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
+use axum::{
+    Router,
+    routing::{
+        get, post
+    }
+};
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "register.html")]
+struct RegisterTemplate {
+    flash_message: String,
+}
+
+#[derive(Template)]
+#[template(path = "Login.html")]
+struct LoginTemplate;
+
+#[derive(Template)]
+#[template(path = "profile.html")]
+struct ProfileTemplate;
+#[derive(Template)]
+#[template(path = "main.html")]
+struct MainTemplate;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
